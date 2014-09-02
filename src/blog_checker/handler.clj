8c21c177d8004b86746aa66926749c8976eb0ecd
Tim Gallant <tim@privy.io>
2014-09-01 20:43:32 -0700

    adding caching headers

--- src/blog_checker/handler.clj
+++ src/blog_checker/handler.clj
@@ -6,7 +6,11 @@
             [blog-checker.utils :refer [get-info]]))
 
 (defroutes app-routes
-  (GET "/" [] (resp/resource-response "index.html" {:root "public"}))
+  (GET "/" req
+       (-> (resp/resource-response "index.html" {:root "public"})
+           (resp/header "Cache-Control" "max-age=604800")
+           (resp/header "Vary" "Accept-Encoding")
+           (resp/header "Content-Type" "text/html; charset=utf-8")))
   (POST "/" [url] (get-info url))
   (route/resources "/")
   (route/not-found "Not Found"))
