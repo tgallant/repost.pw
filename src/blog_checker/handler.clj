71967ba3984a76eeba53155be0a9677b8841ec2f
Tim Gallant <tim@privy.io>
2014-09-01 22:52:13 -0700

    fixing

--- src/blog_checker/handler.clj
+++ src/blog_checker/handler.clj
@@ -5,14 +5,12 @@
             [ring.util.response :as resp]
             [blog-checker.utils :refer [get-info]]))
 
-(def home-page
-  (-> (resp/resource-response "index.html" {:root "public"})
-      (resp/header "Cache-Control" "max-age=604800")
-      (resp/header "Vary" "Accept-Encoding")
-      (resp/header "Content-Type" "text/html; charset=utf-8")))
-
 (defroutes app-routes
-  (GET "/" [] home-page)
+  (GET "/" req
+       (-> (resp/resource-response "index.html" {:root "public"})
+           (resp/header "Cache-Control" "max-age=604800")
+           (resp/header "Vary" "Accept-Encoding")
+           (resp/header "Content-Type" "text/html; charset=utf-8")))
   (POST "/" [url] (get-info url))
   (route/resources "/")
   (route/not-found "Not Found"))
