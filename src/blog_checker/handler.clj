e59926dba227ff93b43ce6b784262f90b85974fc
Tim Gallant <tim@privy.io>
2014-09-01 21:50:03 -0700

    removing needless js

--- src/blog_checker/handler.clj
+++ src/blog_checker/handler.clj
@@ -5,12 +5,14 @@
             [ring.util.response :as resp]
             [blog-checker.utils :refer [get-info]]))
 
+(def home-page
+  (-> (resp/resource-response "index.html" {:root "public"})
+      (resp/header "Cache-Control" "max-age=604800")
+      (resp/header "Vary" "Accept-Encoding")
+      (resp/header "Content-Type" "text/html; charset=utf-8")))
+
 (defroutes app-routes
-  (GET "/" req
-       (-> (resp/resource-response "index.html" {:root "public"})
-           (resp/header "Cache-Control" "max-age=604800")
-           (resp/header "Vary" "Accept-Encoding")
-           (resp/header "Content-Type" "text/html; charset=utf-8")))
+  (GET "/" [] home-page)
   (POST "/" [url] (get-info url))
   (route/resources "/")
   (route/not-found "Not Found"))
--- src/blog_checker/utils.clj
+++ src/blog_checker/utils.clj
@@ -9,56 +9,70 @@
             [clj-time.coerce :as c]
             [blog-checker.sentiment :refer [score-sentence]]))
 
-(defn get-link [link]
+(defn get-link
+  [link]
   (client/get (join "" ["http://hn.algolia.com/api/v1/search?query=" link "&tags=story"])))
 
-(defn get-reddit-link [link]
+(defn get-reddit-link
+  [link]
   (client/get (join "" ["http://www.reddit.com/submit.json?url=" link])))
 
-(defn get-body [link]
+(defn get-body
+  [link]
   (get (get (parse-string (get (get-link link) :body) true) :hits) 0))
 
-(defn get-reddit-body [link]
+(defn get-reddit-body
+  [link]
   (get (get (parse-string (get (get-reddit-link link) :body) true) :data) :children))
 
-(defn get-id [obj]
+(defn get-id
+  [obj]
   (get (get obj :_tags) 2))
 
-(defn get-url [obj]
+(defn get-url
+  [obj]
   (let [id (get-id obj)]
     (if (empty? id) 0
       (join "" ["https://news.ycombinator.com/item?id=" (re-find #"\d+" id)]))))
 
-(defn get-comments [obj]
+(defn get-comments
+  [obj]
   (client/get (join "" ["http://hn.algolia.com/api/v1/search?tags=comment," (get-id obj) "&hitsPerPage=150"])))
 
-(defn get-body-comments [obj]
+(defn get-body-comments
+  [obj]
   (get (parse-string (get (get-comments obj) :body) true) :hits))
 
-(defn get-sentiment [a-vec]
+(defn get-sentiment
+  [a-vec]
   (if (empty? a-vec) "none"
     (for [x (get-body-comments a-vec)]
       (score-sentence (get x :comment_text)))))
 
-(defn avg-sent [a-vec]
+(defn avg-sent
+  [a-vec]
   (if (empty? a-vec) 0
     (/ (reduce + a-vec) (count a-vec))))
 
-(defn sent-classifier [n]
+(defn sent-classifier
+  [n]
   (cond
    (< n 0) "Negative"
    (> n 0) "Positive"
    (= n 0) 0))
 
-(defn parse-date [dt]
+(defn parse-date
+  [dt]
   (let [obj (f/parse (f/formatters :date-time) dt)]
     (f/unparse (f/formatters :rfc822) obj)))
 
-(defn parse-reddit-date [t]
-  (let [dt (.replaceAll (.replaceAll (str t) "E9" "0000") "[.]" "")]
+(defn parse-reddit-date
+  [t]
+  (let [dt (.replaceAll (.replaceAll (str t) "E9" "000") "[.]" "")]
     (f/unparse (f/formatters :rfc822) (c/from-long (Long. dt)))))
 
-(defn posted-first [hn reddit]
+(defn posted-first
+  [hn reddit]
   (let [hn-date (f/parse (f/formatters :date-time) (get hn :created_at))
         reddit-date (for [x reddit]
                       (Long. (.replaceAll (.replaceAll (str (get-in x [:data :created_utc])) "E9" "000") "[.]" "")))
